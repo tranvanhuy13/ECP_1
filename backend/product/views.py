e5a12b8185ac0832a7480de19132121c12562ede
anhtuantran0413 <anhtuantran0413@gmail.com>
2025-11-20 11:54:24 +0700

    feature product view

---
+++ backend/product/views.py
@@ -0,0 +1,16 @@
+from .models import Product
+from rest_framework import status
+from django.shortcuts import render
+from rest_framework.views import APIView
+from .serializers import ProductSerializer
+from rest_framework.response import Response
+from rest_framework import authentication, permissions
+from rest_framework.decorators import permission_classes
+
+
+class ProductView(APIView):
+
+    def get(self, request):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
