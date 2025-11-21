a8564eb92f704fee2f062f3d747ee1995de2dc10
Viet Huong <tranviethuong30102005@gmail.com>
2025-11-21 22:57:16 +0700

    Add Login Page, Product page and constants

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -88,5 +88,5 @@ function NavBar() {
         </header>
     )
 }
-
+//
 export default NavBar
--- frontend/src/pages/ProductDetailsPage.js
+++ frontend/src/pages/ProductDetailsPage.js
@@ -1,160 +1,189 @@
-import React, { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { deleteProduct, getProductDetails } from '../actions/productActions'
-import Message from '../components/Message'
-import { Spinner, Row, Col, Container, Card, Button, Modal } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
-import { CREATE_PRODUCT_RESET, DELETE_PRODUCT_RESET, UPDATE_PRODUCT_RESET, CARD_CREATE_RESET } from '../constants'
-
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { deleteProduct, getProductDetails } from "../actions/productActions";
+import Message from "../components/Message";
+import {
+  Spinner,
+  Row,
+  Col,
+  Container,
+  Card,
+  Button,
+  Modal,
+} from "react-bootstrap";
+import { Link } from "react-router-dom";
+import {
+  CREATE_PRODUCT_RESET,
+  DELETE_PRODUCT_RESET,
+  UPDATE_PRODUCT_RESET,
+  CARD_CREATE_RESET,
+} from "../constants";
 
 function ProductDetailsPage({ history, match }) {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch()
+  // modal state and functions
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  //
+  // product details reducers
+  const productDetailsReducer = useSelector(
+    (state) => state.productDetailsReducer
+  );
+  const { loading, error, product } = productDetailsReducer;
 
-    // modal state and functions
-    const [show, setShow] = useState(false);
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+  // login reducer
+  const userLoginReducer = useSelector((state) => state.userLoginReducer);
+  const { userInfo } = userLoginReducer;
 
-    // product details reducer
-    const productDetailsReducer = useSelector(state => state.productDetailsReducer)
-    const { loading, error, product } = productDetailsReducer
+  // product details reducer
+  const deleteProductReducer = useSelector(
+    (state) => state.deleteProductReducer
+  );
+  const { success: productDeletionSuccess } = deleteProductReducer;
 
-    // login reducer
-    const userLoginReducer = useSelector(state => state.userLoginReducer)
-    const { userInfo } = userLoginReducer
+  useEffect(() => {
+    dispatch(getProductDetails(match.params.id));
+    dispatch({
+      type: UPDATE_PRODUCT_RESET,
+    });
+    dispatch({
+      type: CREATE_PRODUCT_RESET,
+    });
+    dispatch({
+      type: CARD_CREATE_RESET,
+    });
+  }, [dispatch, match]);
 
-    // product details reducer
-    const deleteProductReducer = useSelector(state => state.deleteProductReducer)
-    const { success: productDeletionSuccess } = deleteProductReducer
+  // product delete confirmation
+  const confirmDelete = () => {
+    dispatch(deleteProduct(match.params.id));
+    handleClose();
+  };
 
-    useEffect(() => {
-        dispatch(getProductDetails(match.params.id))
-        dispatch({
-            type: UPDATE_PRODUCT_RESET
-        })
-        dispatch({
-            type: CREATE_PRODUCT_RESET
-        })
-        dispatch({
-            type: CARD_CREATE_RESET
-        })
-    }, [dispatch, match])
+  // after product deletion
+  if (productDeletionSuccess) {
+    alert("Product successfully deleted.");
+    history.push("/");
+    dispatch({
+      type: DELETE_PRODUCT_RESET,
+    });
+  }
 
-    // product delete confirmation
-    const confirmDelete = () => {
-        dispatch(deleteProduct(match.params.id))
-        handleClose()
-    }
+  return (
+    <div>
+      {/* Modal Start*/}
+      <div>
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>
+              <i
+                style={{ color: "#e6e600" }}
+                className="fas fa-exclamation-triangle"
+              ></i>{" "}
+              Delete Confirmation
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            Are you sure you want to delete this product{" "}
+            <em>"{product.name}"</em>?
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="danger" onClick={() => confirmDelete()}>
+              Confirm Delete
+            </Button>
+            <Button variant="primary" onClick={handleClose}>
+              Cancel
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
 
-    // after product deletion
-    if (productDeletionSuccess) {
-        alert("Product successfully deleted.")
-        history.push("/")
-        dispatch({
-            type: DELETE_PRODUCT_RESET
-        })
-    }
+      {/* Modal End */}
 
-    return (
+      {loading && (
+        <span style={{ display: "flex" }}>
+          <h5>Getting Product Details</h5>
+          <span className="ml-2">
+            <Spinner animation="border" />
+          </span>
+        </span>
+      )}
+      {error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
         <div>
+          <Container>
+            <Row>
+              <Col md={6}>
+                <Card.Img variant="top" src={product.image} height="420" />
 
-            {/* Modal Start*/}
-            <div>
-                <Modal show={show} onHide={handleClose}>
-                    <Modal.Header closeButton>
-                        <Modal.Title>
-                            <i style={{ color: "#e6e600" }} className="fas fa-exclamation-triangle"></i>
-                            {" "}
-                            Delete Confirmation
-                        </Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>Are you sure you want to delete this product <em>"{product.name}"</em>?</Modal.Body>
-                    <Modal.Footer>
-                        <Button variant="danger" onClick={() => confirmDelete()}>
-                            Confirm Delete
-                        </Button>
-                        <Button variant="primary" onClick={handleClose}>
-                            Cancel
-                        </Button>
-                    </Modal.Footer>
-                </Modal>
-            </div>
+                {/* Product edit and delete conditions */}
 
-            {/* Modal End */}
+                {userInfo && userInfo.admin ? (
+                  <span style={{ display: "flex" }}>
+                    <button
+                      className="btn mt-2 btn-danger btn-sm button-focus-css"
+                      style={{ width: "100%" }}
+                      onClick={() => handleShow()}
+                    >
+                      Delete Product
+                    </button>
 
-            {loading && <span style={{ display: "flex" }}>
-                <h5>Getting Product Details</h5>
-                <span className="ml-2">
-                    <Spinner animation="border" />
+                    <button
+                      className="ml-2 mt-2 btn btn-primary btn-sm button-focus-css"
+                      onClick={() =>
+                        history.push(`/product-update/${product.id}/`)
+                      }
+                      style={{ width: "100%" }}
+                    >
+                      Edit Product
+                    </button>
+                  </span>
+                ) : (
+                  ""
+                )}
+              </Col>
+
+              <Col sm>
+                <b>{product.name}</b>
+                <hr />
+                <span className="justify-description-css">
+                  <p>{product.description}</p>
                 </span>
-            </span>}
-            {error ? <Message variant='danger'>{error}</Message>
-                :
-                <div>
-                    <Container>
-                        <Row>
-                            <Col md={6}>
-                                <Card.Img variant="top" src={product.image} height="420" />
-
-                                {/* Product edit and delete conditions */}
-
-                                {userInfo && userInfo.admin ?
-                                    <span style={{ display: "flex" }}>
-                                        < button
-                                            className="btn mt-2 btn-danger btn-sm button-focus-css"
-                                            style={{ width: "100%" }}
-                                            onClick={() => handleShow()}
-                                        >Delete Product
-                                        </button>
-
-                                        <button
-                                            className="ml-2 mt-2 btn btn-primary btn-sm button-focus-css"
-                                            onClick={() => history.push(`/product-update/${product.id}/`)}
-                                            style={{ width: "100%" }}
-                                        >Edit Product
-                                        </button>
-                                    </span>
-                                    : ""}
-                            </Col>
-
-                            <Col sm>
-                                <b>{product.name}</b>
-                                <hr />
-                                <span className="justify-description-css">
-                                    <p>{product.description}</p>
-                                </span>
-                                <span style={{
-                                    display: "flex",
-                                    justifyContent: "center",
-                                    border: "1px solid",
-                                    borderColor: "#C6ACE7",
-                                    padding: "2px"
-                                }}>
-                                    Price:<span className="text-success ml-2">₹ {product.price}</span>
-                                </span>
-                            </Col>
-                            <Col sm>
-                                <b>Buy</b>
-                                <hr />
-                                {product.stock ?
-                                    <Link to={`${product.id}/checkout/`}>
-                                        <button className="btn btn-primary">
-                                            <span>Pay with Stripe</span>
-                                        </button>
-                                    </Link>
-                                    :
-                                    <Message variant='danger'>
-                                        Out Of Stock!
-                                    </Message>}
-                            </Col>
-                        </Row>
-
-                    </Container>
-                </div>
-            }
-        </div >
-    )
+                <span
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    border: "1px solid",
+                    borderColor: "#C6ACE7",
+                    padding: "2px",
+                  }}
+                >
+                  Price:
+                  <span className="text-success ml-2">₹ {product.price}</span>
+                </span>
+              </Col>
+              <Col sm>
+                <b>Buy</b>
+                <hr />
+                {product.stock ? (
+                  <Link to={`${product.id}/checkout/`}>
+                    <button className="btn btn-primary">
+                      <span>Pay with Stripe</span>
+                    </button>
+                  </Link>
+                ) : (
+                  <Message variant="danger">Out Of Stock!</Message>
+                )}
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default ProductDetailsPage
+export default ProductDetailsPage;
