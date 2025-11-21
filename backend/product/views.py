bc11ed671aa86567cec0d68b502b9f21fc6170d4
anhtuantran0413 <anhtuantran0413@gmail.com>
2025-11-21 22:16:51 +0700

    feature product create

--- backend/product/views.py
+++ backend/product/views.py
@@ -21,3 +21,39 @@ class ProductDetailView(APIView):
         product = Product.objects.get(id=pk)
         serializer = ProductSerializer(product, many=False)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+class ProductCreateView(APIView):
+
+    permission_classes = [permissions.IsAdminUser]
+
+    def post(self, request):
+        user = request.user
+        data = request.data
+
+        product = {
+            "name": data["name"],
+            "description": data["description"],
+            "price": data["price"],
+            "stock": data["stock"],
+            "image": data["image"],
+        }
+
+        serializer = ProductSerializer(data=product, many=False)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response({"detail": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ProductDeleteView(APIView):
+
+    permission_classes = [permissions.IsAdminUser]
+
+    def delete(self, request, pk):
+        try:
+            product = Product.objects.get(id=pk)
+            product.delete()
+            return Response({"detail": "Product successfully deleted."}, status=status.HTTP_204_NO_CONTENT)
+        except:
+            return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
